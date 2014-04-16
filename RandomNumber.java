55be78272040c826db05d0b6dd489eae07c1bda6
James Perez <xoptoss@gmail.com>
2014-04-15 21:15:36 -0700

    Update RandomNumber.java

--- RandomNumber.java
+++ RandomNumber.java
@@ -7,9 +7,9 @@ public class RandomNumber {
 	Random random = new Random();
 	int answer;
 
-	public int GetANumber_Between_1_and_10(9)+1;
+	public int GetANumber_Between_1_and_10();
 	{
-		answer = random.nextInt();
+		answer = random.nextInt(9)+1;
 		return answer;
 		
 	}
